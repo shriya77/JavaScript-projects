da18857890917a0ca0b4dded2b1796346319b359
Shriya Kalyan <shriyakkx@gmail.com>
2025-01-09 06:13:20 -0600

    Update ketoDietPlanner.js

--- ketoDietPlanner.js
+++ ketoDietPlanner.js
@@ -1,4 +1,4 @@
-// object with arrays containing keto acceptable foods
+// object with arrays for keto acceptable foods
 let foodItems = {
   proteins: ['chicken', 'ham', 'turkey', 'beef'],
   veggies: ['tomato', 'pepper', 'onion', 'avocado', 'broccoli', 'spinach', 'cauliflower'],
@@ -7,19 +7,46 @@ let foodItems = {
   fruits: ['watermelon', 'strawberry', 'berries']
 }
 
-// function that randomly generates a food from the array 
+// generate a random food from each category for user
 let foodGenerator = (foodType) => {
-  randInt = Math.random() * foodType.length;
-  roundedRand = Math.floor(randInt);
+  let randInt = Math.random() * foodType.length;
+  let roundedRand = Math.floor(randInt);
   return foodType[roundedRand];
 }
 
-// output for user with randomly generated food items from each category
+// list out these randomly chosen food items as a meal plan 
+proteinChoice = foodGenerator(foodItems.proteins);
 console.log('Here is your meal!')
 console.log('------------------')
-console.log('Protein: ' + foodGenerator(foodItems.proteins));
+console.log('Protein: ' + proteinChoice);
 console.log('Fat source: ' + foodGenerator(foodItems.fats));
 console.log('Veggies: ' + foodGenerator(foodItems.veggies));
 console.log('Fruit (optional): ' + foodGenerator(foodItems.fruits));
 console.log('Nuts for the crunch: ' + foodGenerator(foodItems.nuts));
+
+// arrays in meals object for holding dishes to recommend to user for each protein type
+let meals = {
+  beef: {
+    dishes: ['keto beef & broccoli', 'ground beef casserole', 'keto beef stroganoff'],
+  },
+  chicken: {
+    dishes: ['garlic butter chicken', 'chicken parm', 'keto crack chicken'],
+  },
+  turkey: {
+    dishes: ['turkey cheese taco', 'turkey skillet', 'egg and turkey mince'],
+  },
+  ham: {
+    dishes: ['ham and cheese bites', 'ham and cauliflower casserole', 'ham soup'],
+  }
+}
+
+// function for randomly generating dish recommendation
+let mealChoice = (proteinChoice) => {
+  let random = Math.floor(Math.random() * 3);
+  return 'Your recommended dish is: ' + meals[proteinChoice].dishes[random];
+}
+
+// outputting recommendation to user
+console.log(mealChoice(proteinChoice));
+
 
